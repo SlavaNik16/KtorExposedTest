e2b4b18f7bdfc2d5b7a546acc42e2b33da242e16
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2024-03-23 22:56:57 +0300

    оптимизация импортов

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,7 +1,9 @@
 package com.example
 
 import com.example.Database.DatabaseFactory.initializeDatabase
-import com.example.plugins.*
+import com.example.plugins.configureMonitoring
+import com.example.plugins.configureSecurity
+import com.example.plugins.configureSerialization
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
