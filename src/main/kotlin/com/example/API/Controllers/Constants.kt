4d290045ff374bdc544b5bbdf0b593517fee6520
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2024-03-25 05:48:30 +0300

    начинаю делать второй контроллер

--- src/main/kotlin/com/example/API/Controllers/Constants.kt
+++ src/main/kotlin/com/example/API/Controllers/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.API.Controllers
 
 class Constants {
-    companion object{
+    companion object {
         const val IS_EMPTY = "Пусто!!!"
         const val NOT_FOUND_USER = "Пользователь не найден!"
         const val GENERAL = "Не правильно введенные данные!"
